2319af858dd8ab32f6116e56614219e577a4b8e2
Bob <b.vandonselaar@student.fontys.nl>
2019-12-30 06:45:38 +0100

    IdentityService implementation (Identity Framework)

---
+++ src/Infrastructure/Identity/ApplicationUser.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Mugger.Infrastructure.Identity
+{
+    public class ApplicationUser : IdentityUser
+    {
+    }
+}
---
+++ src/Infrastructure/Identity/IdentityResultExtensions.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Mugger.Infrastructure.Identity
+{
+    public static class IdentityResultExtensions
+    {
+        public static Result ToApplicationResult(this IdentityResult result)
+        {
+            return result.Succeeded
+                ? Result.Success()
+                : Result.Failure(result.Errors.Select(e => e.Description));
+        }
+    }
+}
